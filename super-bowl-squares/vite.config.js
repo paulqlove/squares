4e135ee3cb3a3d04bd0deab3f1382dc59017ab09
Paul Love <paulqlove@gmail.com>
2025-01-16 16:30:46 -0600

    config fix

--- super-bowl-squares/vite.config.js
+++ super-bowl-squares/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // Replace with your repository name
+  base: '/squares/', // This should match your repo name
 })
